aa2ddac10ddfb8f86b903c2de08c883d0935fdd9
tarcizio502 <tarciziogoncalves502@gmail.com>
2025-05-04 12:37:12 -0300

    Foi add a media queries para celular.

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function nextImage() {
 
   document.getElementById("radio" + count).checked = true;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  let nome = document.querySelector(".nome");
+  let cidade = document.querySelector(".cidade");
+  let telefone = document.querySelector(".telafone");
+  let email = document.querySelector(".email");
+  let arr = [];
+
+  btn.addEventListener("click", () => {
+    const pessoa = {
+      nome: nome.value,
+      cidade: cidade.value,
+      telefone: telefone.value,
+      email: email.value,
+    };
+    arr.push(pessoa);
+    infos.innerHTML = ``;
+    arr.forEach((elemento, index) => {
+      infos.innerHTML += `
+  <p>Nome: ${elemento.nome}</p>
+  <p>Cidade: ${elemento.cidade}</p> 
+  <p>Telefone: ${elemento.telefone}</p> 
+  <p>Email: ${elemento.email}<p/> 
+  `;
+    });
+  });
+});
